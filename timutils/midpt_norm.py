5999b749dbea5965bf7a1fdd060b7537a0342452
Timothy W. Hilton <thilton@ucmerced.edu>
2015-01-19 12:26:30 -0800

    "implement" MidpointNormalize

---
+++ timutils/midpt_norm.py
@@ -0,0 +1,19 @@
+import numpy as np
+from matplotlib.colors import Normalize
+
+class MidpointNormalize(Normalize):
+    """
+    posted by Joe Kington to
+    http://stackoverflow.com/questions/20144529/shifted-colorbar-matplotlib
+    accessed 19 January 2015
+    """
+
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
