aeb43672a805ead9ef4cd42cfb6bbeb85405e97b
Timothy W. Hilton <thilton@ucmerced.edu>
2015-11-05 10:51:13 -0700

    attempting to implement discrete bands, midpoint normalizer
    - this would allow a colorbar with N discrete bands (as opposed to
      continuous variations of colors).
    - This attempt doesn't work quite right because the sort can mismatch
      the midpoint value in [vmin, ..., vmax] from 0.5 in the normalized
      [0.0, ..., 0.5, ... 1.0].

--- timutils/midpt_norm.py
+++ timutils/midpt_norm.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib.colors import Normalize
 
+
 class MidpointNormalize(Normalize):
     """
     posted by Joe Kington to
@@ -8,12 +9,24 @@ class MidpointNormalize(Normalize):
     accessed 19 January 2015
     """
 
-    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+    def __init__(self, vmin=None, vmax=None, midpoint=None,
+                 clip=False, nlevs=9):
         self.midpoint = midpoint
+        self.nlevs = nlevs
         Normalize.__init__(self, vmin, vmax, clip)
 
     def __call__(self, value, clip=None):
         # I'm ignoring masked values and all kinds of edge cases to make a
         # simple example...
-        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+
+        x = np.sort(np.concatenate([np.linspace(start=self.vmin,
+                                                stop=self.vmax,
+                                                num=self.nlevs-1),
+                                    [self.midpoint]]))
+        y = np.sort(np.concatenate([np.linspace(start=0.0,
+                                                stop=1.0,
+                                                num=self.nlevs-1),
+                                    [0.5]]))
+        print 'x, y:', np.dstack([x, y])
+        # x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]  #
         return np.ma.masked_array(np.interp(value, x, y))
