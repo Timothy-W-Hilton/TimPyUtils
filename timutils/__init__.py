afd25efb105b429af71a603fd822f54b00c1d787
Timothy W. Hilton <thilton@ucmerced.edu>
2015-11-09 13:58:29 -0700

    add docstring for the package, ignore auto-generated docs in .gitignore

---
+++ timutils/__init__.py
@@ -0,0 +1,7 @@
+"""
+A collection of short utility functions that I find useful.  Some I've
+written myself and some I've scrounged from the web.
+
+9 Nov 2015
+Timothy W. Hilton <thilton@ucmerced.edu>
+"""
