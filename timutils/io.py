1e55670249494e83d3d16af32399a0bc3778468d
Timothy W. Hilton <thilton@ucmerced.edu>
2016-04-04 10:38:29 -0700

    implement get_temp_filename in io.py

--- timutils/io.py
+++ timutils/io.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import os.path
+import tempfile
 
 
 def delete_if_exists(fname):
@@ -21,3 +22,31 @@ def delete_if_exists(fname):
         os.remove(fname)
         if os.path.exists(fname):
             raise OSError('delete_if_exists failed: file still exists')
+
+
+def get_temp_filename(dir="SCRATCH", prefix='tmpfile_', suffix='.png'):
+    """return a name for a temporary file
+
+    ARGS:
+        dir (string): full path of directory for the temporary
+            file. Default is $SCRATCH.
+        prefix (string): prefix to place before the random string in
+            the filename.  Default is "tmpfile_"
+        prefix (string): suffix to place after the random string in
+            the filename.  Default is ".png"
+
+    RETURNS:
+        a string containing the full path to a temporary file
+
+    EXAMPLE:
+        tmp_name = get_temp_filename(dir='/tmp', suffix='.txt')
+    """
+
+    if dir is "SCRATCH":
+        dir = os.environ['SCRATCH']
+
+    with tempfile.NamedTemporaryFile(dir=dir,
+                                     prefix=prefix, suffix=suffix) as tmpfile:
+        temp_file_name = tmpfile.name
+
+    return temp_file_name
