8c96c94f38ae38551026f9c1d3250ff645456fe5
Timothy W. Hilton <twhilton@ucsc.edu>
2019-09-30 14:34:37 -0600

    catch TypeError when reading active_branch on detached head

--- timutils/git_tools.py
+++ timutils/git_tools.py
@@ -12,7 +12,15 @@ def print_cwd_git_version():
         _repo = git.Repo(search_parent_directories=True)
         _git_sha = _repo.head.object.hexsha
         _git_short_sha = _repo.git.rev_parse(_git_sha, short=7)
-        _git_branch = _repo.active_branch
-        print("On branch {} at rev {}".format(_git_branch, _git_short_sha))
+        try:
+            _git_branch = _repo.active_branch
+        except TypeError as e:
+            if 'detached' in str(e):
+                branch_str = "detached head;"
+            else:
+                raise
+        else:
+            branch_str = "On branch {}".format(_git_branch)
+        print("{} at rev {}".format(branch_str, _git_short_sha))
     except git.InvalidGitRepositoryError:
         print("No git repository detected.")
